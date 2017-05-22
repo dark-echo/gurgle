ae046edec07fe7114bd954c19de9888fd6810440
marcosparks <marcosparks@users.noreply.github.com>
2017-05-22 10:22:43 +0100

    Initial working implementation of the feed.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # gurgle
 Provides for the flow of Elite:Dangerous faction influence data from providers (EDDN, Journal Log) to Google Sheets.
+
+The sheet.js provides a simple web app for Google Sheets that accepts incoming data (POST or GET) and updates the specified sheet with any values that match the column titles (row 1). This can be simply configured through the Script Editor using the Publish -> Deploy as web app... option.
+
+The eddn.py provides for listening to the Elite Dangerous Data Network which provides a ZeroMQ (0MQ) feed of events supplied through various client applications. We specifically listen for the FSDJump events that detail the faction influences in any visited system, parse the JSON to create data that we can then POST to Google Sheet web app.
+
+The file.py provides an equivalent that simply takes a file
+
+TODO:
+ - Configuration file.
+ - Authentication (using simple API application key)
+ - Refactoring to share code between eddn.py/file.py
+ - PowerShell client implementation that monitors player journals (for those not using EDDN feeder apps)
+
---
+++ sheet.js
@@ -0,0 +1,73 @@
+//  1. Define sheet names where data is to be written below
+var INFLUENCE_SHEET_NAME = "Influence";
+         
+//  2. Run > setup
+//
+//  3. Publish > Deploy as web app 
+//    - enter Project Version name and click 'Save New Version' 
+//    - set security level and enable service (most likely execute as 'me' and access 'anyone, even anonymously) 
+//
+//  4. Copy the 'Current web app URL' and post this in your form/script action 
+//
+//  5. Insert column names on your destination sheet matching the parameter names of the data you are passing in (exactly matching case)
+ 
+var SCRIPT_PROP = PropertiesService.getScriptProperties(); // new property service
+ 
+// If you don't want to expose either GET or POST methods you can comment out the appropriate function
+function doGet(e){
+  return handleResponse(e);
+}
+ 
+function doPost(e){
+  return handleResponse(e);
+}
+ 
+function updateInfluence(doc, e) {
+  // store values in the appropriate sheet
+  var sheet = doc.getSheetByName(INFLUENCE_SHEET_NAME);
+     
+  // we'll use headers in row 1 to define attributes to retrieve
+  var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
+  var nextRow = sheet.getLastRow()+1; // get next row
+  var row = [];
+  // loop through the header columns, ignoring undefined entries
+  for (header in headers){
+    if (typeof e.parameter[headers[header]] != "undefined") {
+      row.push(e.parameter[headers[header]]);
+    }
+  }
+  // more efficient to set values as [][] array than individually
+  sheet.getRange(nextRow, 1, 1, row.length).setValues([row]);  
+}
+
+function handleResponse(e) {
+  // prevent concurrent access overwritting data
+  // [1] http://googleappsdeveloper.blogspot.co.uk/2011/10/concurrency-and-google-apps-script.html
+  // we want a public lock, one that locks for all invocations
+  var lock = LockService.getPublicLock();
+  lock.waitLock(30000);  // wait 30 seconds before conceding defeat.
+   
+  try {
+    // set where we write the data
+    var doc = SpreadsheetApp.openById(SCRIPT_PROP.getProperty("key"));
+    // update influence data
+    updateInfluence(doc, e);
+
+    // return json success results
+    return ContentService
+          .createTextOutput(JSON.stringify({"result":"success", "row": nextRow}))
+          .setMimeType(ContentService.MimeType.JSON);
+  } catch(e){
+    // if error return this
+    return ContentService
+          .createTextOutput(JSON.stringify({"result":"error", "error": e}))
+          .setMimeType(ContentService.MimeType.JSON);
+  } finally { //release lock
+    lock.releaseLock();
+  }
+}
+ 
+function setup() {
+    var doc = SpreadsheetApp.getActiveSpreadsheet();
+    SCRIPT_PROP.setProperty("key", doc.getId());
+}
